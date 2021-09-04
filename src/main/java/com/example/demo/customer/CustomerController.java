c2f073cd5bc3c22c239c2716904a9443e52d4605
Stephen <stephenbrady365@gmail.com>
2021-09-04 19:47:49 +0100

    Add POST, PUT, DELETE mappings to controllers

--- src/main/java/com/example/demo/customer/CustomerController.java
+++ src/main/java/com/example/demo/customer/CustomerController.java
@@ -1,14 +1,39 @@
 package com.example.demo.customer;
 
-import com.example.demo.DemoApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class CustomerController {
 
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
     @GetMapping
-    Customer getCustomer() {
-        return new Customer(1L, "James Bond");
+    List<Customer> getCustomer() {
+        return customerService.getCustomer();
+    }
+
+    @PostMapping
+    void createNewCustomer(@RequestBody Customer customer) {
+        System.out.println("POST REQUEST...");
+        System.out.println(customer);
+    }
+
+    @PutMapping
+    void updateCustomer(@RequestBody Customer customer) {
+        System.out.println("PUT REQUEST...");
+        System.out.println(customer);
+    }
+
+    @DeleteMapping(path = "{customerId}")
+    void deleteCustomer(@PathVariable("customerId") Long customerId) {
+
     }
 }
