f6d970abe70185279e7adcbcbf1b2e69888bb61b
Stephen <stephenbrady365@gmail.com>
2021-09-04 19:38:43 +0100

    Add CustomerRepository

--- src/main/java/com/example/demo/customer/CustomerRepository.java
+++ src/main/java/com/example/demo/customer/CustomerRepository.java
@@ -1,4 +1,21 @@
 package com.example.demo.customer;
 
-public class CustomerRepository {
+import org.springframework.stereotype.Repository;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Repository
+public class CustomerRepository implements CustomerRepo {
+
+    @Override
+    public List<Customer> getCustomers() {
+        // TODO connect to real db
+        return Arrays.asList(
+                new Customer(1L, "James Bond", "password123"),
+                new Customer(2L, "Dave Grohl", "password123"),
+                new Customer(3L, "Lady Gaga", "password123"),
+                new Customer(4L, "Beyonce", "password123")
+        );
+    }
 }
